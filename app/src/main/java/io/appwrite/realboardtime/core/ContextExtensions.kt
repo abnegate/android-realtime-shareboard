cc64d11a11bc423373795153e72898d7312b7f85
Jake Barnby <jakeb994@gmail.com>
2021-08-27 17:19:07 +1200

    Fix keyboard dismiss

--- app/src/main/java/io/appwrite/realboardtime/core/ContextExtensions.kt
+++ app/src/main/java/io/appwrite/realboardtime/core/ContextExtensions.kt
@@ -4,9 +4,17 @@ import android.app.Activity
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
 
+fun Context.readAssetFile(fileName: String): String {
+    return assets.open(fileName).bufferedReader().use {
+        it.readText()
+    }
+}
+
 fun Activity.hideSoftKeyBoard() {
     val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
     if (imm?.isAcceptingText == true) {
-        imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        currentFocus?.let {
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 }
